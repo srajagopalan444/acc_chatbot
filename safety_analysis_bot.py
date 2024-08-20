473c952c1398ef0fa811988b4d90b1fa329a8bde
srajagopalan444 <rajagopalan9sudha@gmail.com>
2024-08-20 13:57:49 +0530

    Update safety_analysis_bot.py

--- safety_analysis_bot.py
+++ safety_analysis_bot.py
@@ -4,13 +4,13 @@ st.title('⛑️ Safety Bot ⛑️')
 
 st.write('Hello world!')
 
-if 'messages' not in st.session_state:
-  st.session_state.messages=[]
-
 with st.chat_message(name="Analyst", avatar='👤'):
   st.write("Hi")
 with st.chat_message(name="Assistant"):
   st.write("How many I help you today?")
+
+if 'messages' not in st.session_state:
+  st.session_state.messages=[]
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
