7e661c239f7453f726a5cee893798193c8bc6d56
srajagopalan444 <rajagopalan9sudha@gmail.com>
2024-08-20 12:29:45 +0530

    Update safety_analysis_bot.py

--- safety_analysis_bot.py
+++ safety_analysis_bot.py
@@ -4,5 +4,7 @@ st.title('⛑️ Safety Bot ⛑️')
 
 st.write('Hello world!')
 
+with st.chat_message(name="Assistant"):
+  st.write("How many I help you today?")
 with st.chat_message(name="Analyst", avatar='👤'):
   st.write("Test")
