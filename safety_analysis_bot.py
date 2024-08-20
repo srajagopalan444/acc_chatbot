3a02a039c3bcd3a00ad32462f1d9d0f632b4bd38
srajagopalan444 <rajagopalan9sudha@gmail.com>
2024-08-20 16:15:21 +0530

    Update safety_analysis_bot.py

--- safety_analysis_bot.py
+++ safety_analysis_bot.py
@@ -31,11 +31,12 @@ if prompt := st.chat_input("Enter the description of the incident..."):
         st.markdown(prompt)
     # Add user message to chat history
     st.session_state.messages.append({"role": "Analyst", "content": prompt})
-
+  with st.chat_message("user"):
+        st.markdown(prompt)
 
 
 with st.chat_message("assistant"):
-    response1 = st.write_stream(response_generator())
+    response = st.write_stream(response_generator())
 # Add assistant response to chat history
-st.session_state.messages.append({"role": "assistant", "content": response1})
+st.session_state.messages.append({"role": "assistant", "content": response})
 
